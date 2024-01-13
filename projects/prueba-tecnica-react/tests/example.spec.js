d964aa9c5aa7310528c03cea409a9e565c603a48
CascoArcilla <normon1230@gmail.com>
2024-01-13 15:13:24 -0600

    delete test example to prueba react

--- projects/prueba-tecnica-react/tests/example.spec.js
+++ projects/prueba-tecnica-react/tests/example.spec.js
@@ -9,10 +9,20 @@ test("app shows random fac and image", async ({ page }) => {
 
   const text = await page.getByRole("paragraph");
   const image = await page.getByRole("img");
+  const button = await page.getByRole("button");
 
   const textContent = await text.textContent();
   const imageSrc = await image.getAttribute("src");
 
   await expect(textContent?.length).toBeGreaterThan(0);
   await expect(imageSrc?.startsWith(CAT_PREFIX_URL)).toBeTruthy();
+
+  await button.click();
+
+  const newTextContent = await text.textContent();
+  const newImageSrc = await image.getAttribute("src");
+
+  await expect(newTextContent).toBe(textContent);
+  await expect(newImageSrc).toBe(imageSrc);
+  await expect(newImageSrc?.startsWith(CAT_PREFIX_URL)).toBeTruthy();
 });
