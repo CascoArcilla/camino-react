e73e42770f5723834730e957e733604572e00330
CascoArcilla <normon1230@gmail.com>
2024-02-02 14:03:09 -0600

    Create Route:apply lasy to pages

--- projects/07-create-route-react/src/App.jsx
+++ projects/07-create-route-react/src/App.jsx
@@ -1,10 +1,12 @@
+import { Suspense, lazy } from "react";
 import Router from "./components/Router.jsx";
 import "./App.css";
 import Page404 from "./pages/Page404.jsx";
-import Home from "./pages/Home.jsx";
-import About from "./pages/About.jsx";
 import SearchPage from "./pages/SearchPage.jsx";
 import Route from "./components/Route.jsx";
+
+const Home = lazy(() => import("./pages/Home.jsx"));
+const About = lazy(() => import("./pages/About.jsx"));
 
 const appRoutes = [
   {
@@ -16,10 +18,12 @@ const appRoutes = [
 export default function App() {
   return (
     <main>
-      <Router routes={appRoutes} defaultComponent={Page404}>
-        <Route path="/" Component={Home} />
-        <Route path="/about" Component={About} />
-      </Router>
+      <Suspense fallback={<div>Loading...</div>}>
+        <Router routes={appRoutes} defaultComponent={Page404}>
+          <Route path="/" Component={Home} />
+          <Route path="/about" Component={About} />
+        </Router>
+      </Suspense>
     </main>
   );
 }
--- projects/07-create-route-react/src/pages/About.jsx
+++ projects/07-create-route-react/src/pages/About.jsx
@@ -5,7 +5,7 @@ export default function About() {
     <>
       <h1>About</h1>
       <img
-        src="https://scontent.fjal3-1.fna.fbcdn.net/v/t39.30808-6/380246429_3596964597206290_8419407556614345616_n.jpg?_nc_cat=108&ccb=1-7&_nc_sid=dd5e9f&_nc_eui2=AeFqq8wyflXKul5wwDQx-nJ9ZxuK8uDvTrNnG4ry4O9Os_j2Z0zDkyXAtUGMmMnRBwb0z1LlInO3U5KzdHc6j-NJ&_nc_ohc=jSwgMoDYLZQAX_mc6yu&_nc_ht=scontent.fjal3-1.fna&oh=00_AfDcYRk9rGuGDlkSUrw_5RJjdBkFGKebwb8-mG7DBxbk-g&oe=65C2175B"
+        src="https://cdn.redcanina.es/wp-content/uploads/2014/10/disfraz-de-perro-de-morsa.jpg"
         alt="Foto mia"
         style={{
           height: "400px",
